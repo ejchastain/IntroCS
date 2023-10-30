45ce1908344df68426bbaa995fbcdfc7406daad7
ejchastain <117233058+ejchastain@users.noreply.github.com>
2023-10-30 17:28:42 -0500

    Update pc_startercode_c2.rkt
    Updated the code to finish out the add-accidents function. It now renders everything (player character, enemy, items [in principle]). 
    
    I also extensively commented everything.

--- pc_startercode_c2.rkt
+++ pc_startercode_c2.rkt
@@ -3,7 +3,7 @@
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname pc_startercode_c2) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "dir.rkt" "teachpack" "htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
 (require 2htdp/planetcute)
 
-       ; list of lists, each list is a stack of images
+       ; list of lists, each list is a stack of images, used to define our game terrain
  (define test_stack
    (list (list wall-block-tall stone-block)
          (list stone-block stone-block stone-block stone-block)
@@ -13,13 +13,28 @@
          (list   grass-block dirt-block dirt-block))
    )
 
+; The Essential aspects of the game-world
+; What does the player character look like? PLAYER_IMAGE
+; What does the enemy look like? ENEMY_IMAGE
+; What does the Game World terrain look like? STACKS
 (define PLAYER_IMAGE character-cat-girl)
+(define ENEMY_IMAGE enemy-bug)
 (define STACKS test_stack)
+
+; The Accidental aspects of the game-world
 
 ; Pc:StructOf(Number)
 ; Player character information
 ; s_idx is stack index of the player character
 (define-struct Pc [s_idx])
+
+; E:StructOf(Number)
+; Enemy information
+(define-struct E [s_idx])
+
+; It:StructOf(Number x Image)
+; Item information (stack index, image item) 
+(define-struct It [s_idx img])
 
 ; Ac:StructOf(Pc,ListOf(E),ListOf(It))
 ; accidents of the game world
@@ -28,20 +43,29 @@
 ; loi is a list of item structs (accidents of the items)
 (define-struct Ac [pc loe loi])
 
+; set-loe:Ac x ListOf(E) -> Ac
+; Sets the loe field of ac to the list of E structs new_loe
+; and then returns the modified ac
 (define set-loe
-       (lambda [ac new_loe]
+       (λ [ac new_loe]
                (make-Ac (Ac-pc ac) new_loe (Ac-loi ac))
        )
 )
 
+; set-loi:Ac x ListOf(It) -> Ac
+; Sets the loi field of ac to the list of It structs new_loi
+; and then returns the modified ac
 (define set-loi
-       (lambda [ac new_loi]
+       (λ [ac new_loi]
                (make-Ac (Ac-pc ac) (Ac-loe ac) new_loi )
        )
 )
 
+; set-pc:Ac x Pc -> Ac
+; Sets the pc field of ac to the Pc struct new_pc
+; and then returns the modified ac
 (define set-pc
-       (lambda [ac new_pc]
+       (λ [ac new_pc]
                (make-Ac new_pc (Ac-loe ac) (Ac-loe ac) )
        )
 )
@@ -50,6 +74,12 @@
 ; WS:StructOf(ListOf(ListOf(Image)))
 ; ac: accidents of the gameworld
 (define-struct WS [ac])
+
+; The Initial World State
+(define init-WS (make-WS (make-Ac (make-Pc 2) (list (make-E 3) (make-E 6)) empty)))
+
+
+; Helper functions for draw
 
     ; stack : ListOf(Image) -> Image
     ; stacks 'imgs' on each other, separated by 40 pixels
@@ -64,6 +94,7 @@
   )
 
 ; draw-stacks:ListOf(ListOf(Image)) -> Image
+; draws all of the stacks in a list of stacks (a stack is a list of images)
 (define draw-stacks
   (λ [loloi]
     (cond
@@ -77,15 +108,8 @@
     )
   )
 
-
- ; Brainstorming: 
- ; check the first within the stack so that we can find the character
- ; check all the stacks for the character
- ; keep track of the stack containing the character (character stack index)
- ; need to check if its possible to move left or right (can't move left on first stack)
- ; make new ws which allows us to place 
-
  ; add-image:Number x ListOf(ListOf(Image)) -> ListOf(ListOf(Image))
+; add-image just adds an image at stack index idx to the list of lists of images stacks
 (define add-image
   (λ [idx stacks img]
     (cond
@@ -95,45 +119,41 @@
     )
   )
  
-(define init-WS (make-WS (make-Ac (make-Pc 2) empty empty)))
-
-; add-accidents:Ac -> ListOf(ListOf(Image))
+; add-accidents:Ac x ListOf(ListOf(Image)) -> ListOf(ListOf(Image))
+; draws all of the accidents ac on the terrain stacks stks 
+; Approach: as suggested by ben anderson, try to recurse on the non-empty lists
+; of enemies and lists of items, adding the different objects to the terrain (at the
+; corresponding stack index s_idx) as you go. Then after you are done with the list 
+; fields, draw the player character. 
 (define add-accidents
-  (λ [ac]
+  (λ [ac stacks]
      (cond
-       [(cons? (Ac-loe ac)) (add-image (first (Ac-loe ac)) (add-accidents (set-loe ac (rest (Ac-loe ac))))) ]
-       [(cons? (Ac-loi ac)) (add-accidents (set-loe ac (rest (Ac-loe ac))))]
-
-    (add-image (Pc-s_idx (Ac-pc ac)) STACKS PLAYER_IMAGE)
+       [(cons? (Ac-loe ac))
+          (add-accidents (set-loe ac (rest (Ac-loe ac)))
+                         (add-image
+                          (E-s_idx (first (Ac-loe ac))) stacks ENEMY_IMAGE))]
+       [(cons? (Ac-loi ac))
+          (add-accidents (set-loi ac (rest (Ac-loi ac)))
+                         (add-image
+                          (It-s_idx (first (Ac-loi ac))) stacks
+                          (It-img (first (Ac-loi ac))))
+                         )]
+     ;  [(cons? (Ac-loi ac)) (add-accidents (set-loe ac (rest (Ac-loe ac))))]     
+    [else (add-image (Pc-s_idx (Ac-pc ac)) stacks PLAYER_IMAGE)]
       )
-    )
-  
-
-; draw: WS -> Image
-(define draw
-  (λ [ws]
-    (draw-stacks (add-accidents (WS-ac ws)))
     )
   )
 
-
-
-
-
-
-
-
-              
-    (beside/align
-       "bottom"
-       (stack (list wall-block-tall stone-block))
-       (stack (list 
-                    stone-block stone-block
-                    stone-block stone-block))
-       water-block
-       (stack (list character-cat-girl grass-block dirt-block))
-       (stack (list  grass-block dirt-block dirt-block)))
-
+; draw: WS -> Image
+; draws the worldstate ws by outputting its corresponding picture as output
+; Approach: Use add-accidents with the essential STACKS game-world as the background
+; add-accidents will just add all of the different images from the accidents structure
+; to the stacks list of lists of images to get the final image to draw. 
+(define draw
+  (λ [ws]
+    (draw-stacks (add-accidents (WS-ac ws) STACKS))
+    )
+  )
     
 
 
